fe72b2005b7d90e161e6221084c1501c745e2d7a
HEMED ARTHUMAN <khahidu1@gmail.com>
2022-05-20 06:29:54 -0700

    Lab1 Assignment

---
+++ src/kazi.java
@@ -0,0 +1,18 @@
+public class kazi {
+
+        public static void main(String[] args){
+            double a=-9.81;
+            double t=10;
+            double Vi=0;
+            double Xi=0;
+            double Xt;
+
+            Xt = 0.5*a*t*t + Vi*t + Xi;
+            System.out.println("the value of position of object after falling is Xt is:"+Xt+"m");
+
+
+        }
+
+    }
+
+
